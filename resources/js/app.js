37fdb5f373c8675d9b2e0eca337306dcf73fae9b
Mousouba <mousouba@nan.ci>
2020-10-24 12:45:17 +0000

    vuex init

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,9 @@
 require('./bootstrap');
 import  router from './router'
+import Vuex from 'vuex'
+
 window.Vue = require('vue');
+Vue.use(Vuex);
 
 Vue.component('wrapper-component', require('./components/WrapperComponent.vue').default);
 
