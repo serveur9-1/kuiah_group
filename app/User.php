101ecc1b00540a34573a2b74163e486f09dba6be
Mousouba <mousouba@nan.ci>
2020-11-14 13:29:25 +0000

    add user friend table

--- app/User.php
+++ app/User.php
@@ -64,4 +64,23 @@ class User extends Authenticatable
     {
         return $this->belongsToMany('App\Domain');
     }
+
+    // friends
+    public function friends()
+    {
+        $friends = $this->belongsToMany('User', 'friend_user', 'user_id', 'friend_id');
+        return $friends;
+    }
+    public function add_friend($friend_id)
+    {
+        $this->friends()->sync($friend_id);   // add friend
+        $friend = User::find($friend_id);       // find your friend, and...
+        $friend->friends()->attach($this->id);  // add yourself, too
+    }
+    public function remove_friend($friend_id)
+    {
+        $this->friends()->detach($friend_id);   // remove friend
+        $friend = User::find($friend_id);       // find your friend, and...
+        $friend->friends()->detach($this->id);  // remove yourself, too
+    }
 }
