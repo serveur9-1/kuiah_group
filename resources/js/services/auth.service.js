f89f696dbdf9920a5981dea4e4fd991226159e45
serveur9-1 <ayola.sande@uvci.edu.ci>
2020-11-20 16:38:08 +0000

    My first commit

--- resources/js/services/auth.service.js
+++ resources/js/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://kuiah-finance.herokuapp.com/login';
+const API_URL = 'http://kuiah-finance.herokuapp.com/api/v1/users/login';
 
 class AuthService {
   login(user) {
