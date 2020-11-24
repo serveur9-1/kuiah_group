5d02a0ada2f83e8a49b4f05f6eb10a4eb5bf42c5
Mousouba <mousouba@nan.ci>
2020-11-24 16:08:54 +0000

    Reset password and send mail ok

---
+++ resources/views/auth/reset_failure.blade.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel Starter</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+    <!-- Styles -->
+    <style>
+        html, body {
+            background-color: #f1f1f1;
+            color: #636b6f;
+            font-family: 'Nunito', sans-serif;
+            font-weight: 200;
+            height: 100vh;
+            margin: 0;
+        }
+        .full-height {
+            height: 100vh;
+        }
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+            flex-direction: column;
+        }
+        .position-ref {
+            position: relative;
+        }
+        input {
+            padding: 10pt;
+            width: 60%;
+            font-size: 15pt;
+            border-radius: 5pt;
+            border: 1px solid lightgray;
+            margin: 10pt;
+        }
+        .form-container {
+            display: flex;
+            flex-direction: column;
+            width: 60%;
+            align-items: center;
+            margin: 20pt;
+            border: 1px solid lightgray;
+            padding: 20pt;
+            border-radius: 5pt;
+            background: white;
+        }
+        button {
+            border-radius: 5pt;
+            padding: 10pt 14pt;
+            background: white;
+            border: 1px solid gray;
+            font-size: 14pt;
+            margin: 20pt;
+        }
+        button:hover {
+            background: lightgray;
+        }
+    </style>
+</head>
+<body>
+<div class="flex-center position-ref full-height">
+    <img width="150" src="{{asset("images/medias/failure.gif")}}" alt="" srcset="">
+    @if(Session::has('msg'))
+        <h1>{{ Session::get('msg')}}</h1>
+    @endif
+</div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::view('forgot_password', 'auth.reset_password')->name('password.reset');
+
+Route::get('/forgot_success', function () {
+    return view('auth.reset_succed');
+})->name('password.success');
+
+Route::get('/forgot_failure', function () {
+    return view('auth.reset_failure');
+})->name('password.failure');
+
+Route::post('password/reset', 'v1\AuthController@reset')->name('reset.post');
+
 Route::get('/{any}', function () {
     return view('welcome');
 })->where('any', '.*');
